c94d65ee518d4f1b590dd5ed409d6d4e5004c957
Andre-Sampaio-Filho <andre.claudino04@gmail.com>
2024-10-08 21:42:43 -0300

    adicionando novo bodybuilder

--- js/app.js
+++ js/app.js
@@ -1,40 +1,88 @@
-function adicionar(){
+// falso banco de dados de clientesm, em memória RAM
+var clientes = []
+
+function adicionar() {
     mostrarModal()
 }
 
-function salvar(){
+function salvar() {
     let nome = document.getElementById("nome").value
+    let cpf = document.getElementById("cpf").value
     let peso = document.getElementById("peso").value
     let altura = document.getElementById("altura").value
     let dataNascimento = document.getElementById("dataNascimento").value
-    let cpf = document.getElementById("cpf").value
 
-    if (nome == "" || peso == "" || altura == "" || dataNascimento == "" || cpf == ""){
+
+    let novoBodyBuilder = {
+        nome: nome,
+        cpf: cpf,
+        peso: peso,
+        altura: altura,
+        dataNascimento: dataNascimento,
+    }
+
+    clientes.push(novoBodyBuilder)
+
+
+    if (nome == "" || peso == "" || altura == "" || dataNascimento == "" || cpf == "") {
         alert("Preencha todos os campos!")
         return false
     }
+
+
     mostrarLoading()
-    setTimeout (function () {
+    setTimeout(function () {
         ocultarLoading()
         ocultarModal()
+        alert("Bodybuilder adicionado com sucesso!")
+        document.getElementById("form").reset()
+        atualizarLista()
     }, 1500)
-    return false
-    }
 
-    function mostrarLoading() {
-        document.getElementById("loading").style.display = "block";
+
+
+
+    return false
+}
+function atualizarLista() {
+
+    let tbody = document.getElementsByTagName("tbody")[0]
+    tbody.innerHTML = ""
+
+    for (let i = 0; i < clientes.length; i++) {
+        let cliente = clientes[i]
+        let linhaTabela = document.createElement("tr")
+        linhaTabela.innerHTML = `
+                <td>${cliente.cpf}</td>
+                <td>${cliente.nome}</td>
+                <td>${cliente.peso}Kg</td>
+                <td>${cliente.altura}m</td>
+                <td>${cliente.dataNascimento}</td>
+                <td>
+                    <button class="alterar" onclick="mostrarModal()">Alterar</button>
+                    <button class="excluir" onclick="excluir()">Excluir</button>
+                </td>
+        `
+
+        tbody.appendChild(linhaTabela)
     }
-    function ocultarLoading() {
-        document.getElementById("loading").style.display = "none";
+}
+
+function mostrarLoading() {
+    document.getElementById("loading").style.display = "block";
+}
+function ocultarLoading() {
+    document.getElementById("loading").style.display = "none";
+}
+function mostrarModal() {
+    document.getElementById("modal").style.display = "flex";
+    document.getElementById("form").reset()
+}
+function ocultarModal() {
+    document.getElementById("modal").style.display = "none";
+}
+function excluir() {
+    if (confirm("Tem certeza que deseja excluir?")) {
+        alert("Excluído com sucesso!")
     }
-    function mostrarModal(){
-        document.getElementById("modal").style.display = "flex";
-    }
-    function ocultarModal(){
-        document.getElementById("modal").style.display = "none";
-    }   
-    function excluir(){
-        if(confirm("Tem certeza que deseja excluir?")){
-            alert("Excluído com sucesso!")
-    }
-    }
+}
