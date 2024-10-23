335dcd6fe8fe2ba0a99deb08a8753b27d17a4cd4
Andre-Sampaio-Filho <andre.claudino04@gmail.com>
2024-10-22 22:13:24 -0300

    Adicionando a parte de Update do codigo

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
-// falso banco de dados de clientesm, em memória RAM
+// falso banco de dados de clientes, em memória RAM
 var clientes = []
+var clienteAlterado = null
 
 function adicionar() {
+    clienteAlterado = null
     mostrarModal()
+    limparFormulario()
 }
 
 function salvar() {
@@ -11,6 +14,7 @@ function salvar() {
     let peso = document.getElementById("peso").value
     let altura = document.getElementById("altura").value
     let dataNascimento = document.getElementById("dataNascimento").value
+    let sapato = document.getElementById("sapato").value
 
 
     let novoBodyBuilder = {
@@ -19,26 +23,51 @@ function salvar() {
         peso: peso,
         altura: altura,
         dataNascimento: dataNascimento,
+        sapato: sapato
     }
 
-    clientes.push(novoBodyBuilder)
 
 
-    if (nome == "" || peso == "" || altura == "" || dataNascimento == "" || cpf == "") {
+
+    if (nome == "" || peso == "" || altura == "" || dataNascimento == "" || cpf == "" || sapato == "") {
         alert("Preencha todos os campos!")
         return false
     }
 
+    //se clienteAlterado == null, então está adicionando um novo cliente
+    if (clienteAlterado == null) {
 
-    mostrarLoading()
-    setTimeout(function () {
-        ocultarLoading()
-        ocultarModal()
-        alert("Bodybuilder adicionado com sucesso!")
-        limparFormulario()
-        atualizarLista()
-    }, 1500)
 
+        //adiciona o bodyBuilder na lista de clientes
+        clientes.push(novoBodyBuilder)
+
+
+        mostrarLoading()
+        setTimeout(function () {
+            ocultarLoading()
+            ocultarModal()
+            alert("Bodybuilder adicionado com sucesso!")
+            limparFormulario()
+            atualizarLista()
+        }, 1500)
+    } else {//se não, está alterando um cliente
+        clienteAlterado.nome = nome
+        clienteAlterado.peso = peso
+        clienteAlterado.altura = altura
+        clienteAlterado.dataNascimento = dataNascimento
+        clienteAlterado.sapato = sapato
+
+
+
+        mostrarLoading()
+        setTimeout(function () {
+            ocultarLoading()
+            ocultarModal()
+            alert("Bodybuilder alterado com sucesso!")
+            limparFormulario()
+            atualizarLista()
+        }, 1500)
+    }
 
 
 
@@ -63,8 +92,9 @@ function atualizarLista() {
                 <td>${cliente.peso}Kg</td>
                 <td>${cliente.altura}m</td>
                 <td>${cliente.dataNascimento}</td>
+                <td>${cliente.sapato}</td>
                 <td>
-                    <button class="alterar" onclick="mostrarModal()">Alterar</button>
+                    <button class="alterar" onclick="alterar('${cliente.cpf}')">Alterar</button>
                     <button class="excluir" onclick="excluir('${cliente.cpf}')">Excluir</button>
                 </td>
         `
@@ -97,5 +127,21 @@ function excluir(cpf) {
             }
         }
 
+    }
+}
+function alterar(cpf) {
+    //busca o cliente a ser alterado
+    for (let i = 0; i < clientes.length; i++) {
+        let cliente = clientes[i]
+        if (cliente.cpf == cpf) {
+            document.getElementById("nome").value = cliente.nome
+            document.getElementById("cpf").value = cliente.cpf
+            document.getElementById("peso").value = cliente.peso
+            document.getElementById("altura").value = cliente.altura
+            document.getElementById("dataNascimento").value = cliente.dataNascimento
+            document.getElementById("sapato").value = cliente.sapato
+            clienteAlterado = cliente
+            mostrarModal()
+        }
     }
 }
